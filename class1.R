f4c5ade2fe5ebb2b1421c6b1041114860c0337a8
rahul_85 <113101957+h20220841@users.noreply.github.com>
2022-11-25 22:37:14 +0530

    Add files via upload
    1st class of introduction of the R in Statistics class.

---
+++ class1.R
@@ -0,0 +1,52 @@
+x_1<- "Rahul Kumar"
+x_1
+TRUE * 5
+FALSE *5
+
+P<-TRUE
+class(P)
+
+2==3
+2!=3
+2<3
+
+p1 <- c(1,2,3)
+class(p1)
+p1
+
+p2<-c('Rahul','Yogi','Abhilash')
+p2
+
+p3<-p1 +3
+p3
+
+d<- c(1,2,3,4)
+w<- c(10,20)
+d+w
+
+
+p4<- c(1,2,3)
+p5<- c(1,2)
+p4+p5
+
+p6 <- c(22,23,24,25)
+p6[c(1,3)]
+p6[1:2]
+p6[2]
+p6[1:5]
+#p6[:-1]
+
+dd = 1:30
+dd
+
+dd1 = c("Rahul", 24, "M")
+dd1
+class(dd1)
+
+a_13<-c('fd','hd','phd')
+a_13f=as.factor(a_13)
+class(a_13)
+class(a_13f)
+
+factor(p<-c('FD', 'HD', 'PhD'))
+class(p)
